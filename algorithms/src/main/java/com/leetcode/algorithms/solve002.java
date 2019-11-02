af700b99818b365b9e50a21ffcfc057eaf46dd1c
David <imchenway@gmail.com>
2019-11-02 23:43:50 +0800

    solve

--- algorithms/src/main/java/com/leetcode/algorithms/solve002.java
+++ algorithms/src/main/java/com/leetcode/algorithms/solve002.java
@@ -40,10 +40,26 @@ public class solve002 {
         *   2、从最后一位存入listNode
         *   3、遍历输出
         * 复杂度：
-        *   时间复杂度：
-        *   空间复杂度：
+        *   时间复杂度：O(max(m,n))
+        *   空间复杂度：O(max(m,n))
         * */
-        return null;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
     
     
