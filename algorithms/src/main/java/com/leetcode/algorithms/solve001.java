44a8d743de28191beb504888a08167cf94485a54
DavidChan <imchenway@gmail.com>
2021-01-21 16:46:12 +0800

    solve002

--- algorithms/src/main/java/com/leetcode/algorithms/solve001.java
+++ algorithms/src/main/java/com/leetcode/algorithms/solve001.java
@@ -4,32 +4,30 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 /**
- * @Program: leetCode
- * @Description: twoSum 两数之和
- * @Author: David Chan
- * @createTime 2019-10-15 19:56
- * @Version: 1.0.0
+ * twoSum 两数之和
+ *
+ * @author David Chan
  */
 public class solve001 {
     /*
     两数之和：
-        给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+        给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
         你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
     示例:
         给定 nums = [2, 7, 11, 15], target = 9
         因为 nums[0] + nums[1] = 2 + 7 = 9
         所以返回 [0, 1]
     * */
-    
+
     private static int[] twoSum(int[] nums, int target) {
         /*
-        * 解法：
-        *   1、用数组中的每个索引依次与其他索引计算；
-        *   2、结果如果符合target的要求并且索引不为同一索引，则返回。
-        * 复杂度
-        *   时间复杂度：O(n*n)
-        *   空间复杂度：O(1)
-        * */
+         * 解法：
+         *   1、用数组中的每个索引依次与其他索引计算；
+         *   2、结果如果符合target的要求并且索引不为同一索引，则返回。
+         * 复杂度
+         *   时间复杂度：O(n*n)
+         *   空间复杂度：O(1)
+         * */
         for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target && i != j) {
@@ -38,21 +36,21 @@ public class solve001 {
                 }
             }
         }
-        throw  new IllegalArgumentException("IllegalArgumentException");
+        throw new IllegalArgumentException("IllegalArgumentException");
     }
-    
-    private static int[] twoSumHashMap(int[] nums , int target) {
+
+    private static int[] twoSumHashMap(int[] nums, int target) {
         /*
-        * 解法：
-        *   1、遍历nums获取一个key为nums值，value为nums索引的Map
-        *   2、再次遍历nums，如果target减去nums[i]等于Map的某个Key，则返回该key对应的value（索引）
-        * 复杂度：
-        *   时间复杂度：O(n)
-        *   空间复杂度：O(n)
-        * 问题：
-        *   1、containsKey() hash冲突后时间复杂度可能提升为o(n*n)?
-        *       数字的hashCode为它本身,不可能存在hash碰撞
-        * */
+         * 解法：
+         *   1、遍历nums获取一个key为nums值，value为nums索引的Map
+         *   2、再次遍历nums，如果target减去nums[i]等于Map的某个Key，则返回该key对应的value（索引）
+         * 复杂度：
+         *   时间复杂度：O(n)
+         *   空间复杂度：O(n)
+         * 问题：
+         *   1、containsKey() hash冲突后时间复杂度可能提升为o(n*n)?
+         *       数字的hashCode为它本身,不可能存在hash碰撞
+         * */
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             hashMap.put(nums[i], i);
@@ -63,9 +61,9 @@ public class solve001 {
                 return new int[]{i, hashMap.get(resultInt)};
             }
         }
-        throw  new IllegalArgumentException("IllegalArgumentException");
+        throw new IllegalArgumentException("IllegalArgumentException");
     }
-    
+
     private static int[] twoSumOneHashMap(int[] nums, int target) {
         /*
          * 解法：
@@ -84,9 +82,9 @@ public class solve001 {
             }
             hashMap.put(nums[i], i);
         }
-        throw  new IllegalArgumentException("IllegalArgumentException");
+        throw new IllegalArgumentException("IllegalArgumentException");
     }
-    
+
     public static void main(String[] args) {
         int[] nums = {2, 5, 5, 11};
         int[] twoSum = twoSum(nums, 10);
--- algorithms/src/main/java/com/leetcode/algorithms/solve002.java
+++ algorithms/src/main/java/com/leetcode/algorithms/solve002.java
@@ -5,34 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * @Program: leetCode
- * @Description: 两数相加
- * @Author: David Chan
- * @createTime 2019-10-18 10:23
- * @Version: 1.0.0
+ * 两数相加
+ *
+ * @author David Chan
  */
 public class solve002 {
     /*
     两数相加：
-        给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+        给出两个非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
         如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
-        您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+        您可以假设除了数字 0 之外，这两个数都不会以 0开头。
     示例：
         输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
         输出：7 -> 0 -> 8
         原因：342 + 465 = 807
     * */
-    
+
     // Definition for singly-linked list.
     private static class ListNode {
         int val;
         ListNode next;
-        
+
         ListNode(int x) {
             val = x;
         }
     }
-    
+
     private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode result = dummyHead;
@@ -56,43 +54,43 @@ public class solve002 {
         }
         return dummyHead.next;
     }
-    
-    
+
+
     public static int[] stringToIntegerArray(String input) {
         input = input.trim();
         input = input.substring(1, input.length() - 1);
         if (input.length() == 0) {
             return new int[0];
         }
-        
+
         String[] parts = input.split(",");
         int[] output = new int[parts.length];
-        for(int index = 0; index < parts.length; index++) {
+        for (int index = 0; index < parts.length; index++) {
             String part = parts[index].trim();
             output[index] = Integer.parseInt(part);
         }
         return output;
     }
-    
+
     public static ListNode stringToListNode(String input) {
         // Generate array from the input
         int[] nodeValues = stringToIntegerArray(input);
-        
+
         // Now convert that list into linked list
         ListNode dummyRoot = new ListNode(0);
         ListNode ptr = dummyRoot;
-        for(int item : nodeValues) {
+        for (int item : nodeValues) {
             ptr.next = new ListNode(item);
             ptr = ptr.next;
         }
         return dummyRoot.next;
     }
-    
+
     public static String listNodeToString(ListNode node) {
         if (node == null) {
             return "[]";
         }
-        
+
         String result = "";
         while (node != null) {
             result += Integer.toString(node.val) + ", ";
@@ -100,6 +98,7 @@ public class solve002 {
         }
         return "[" + result.substring(0, result.length() - 2) + "]";
     }
+
     // [2,4,3]
     // [5,6,4]
     public static void main(String[] args) throws IOException {
@@ -109,11 +108,11 @@ public class solve002 {
             ListNode l1 = stringToListNode(line);
             line = in.readLine();
             ListNode l2 = stringToListNode(line);
-            
-            ListNode ret = new solve002().addTwoNumbers(l1, l2);
-            
+
+            ListNode ret = addTwoNumbers(l1, l2);
+
             String out = listNodeToString(ret);
-            
+
             System.out.print(out);
         }
     }
